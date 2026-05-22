a2a7670d5ba6bd549eea3441cbc6dfb393238d92
Max Rupplin <mearvk@outlook.com>
2026-05-22 10:38:05 -0400

    Latest...

---
+++ src/BlackMorning.java
@@ -0,0 +1,9 @@
+public class BlackMorning
+{
+    public String note = "black.morning";
+
+    public BlackMorning()
+    {
+
+    }
+}
---
+++ src/safety/SafetyRoute7520.java
@@ -0,0 +1,11 @@
+package safety;
+
+public class SafetyRoute7520
+{
+    public String images = "";
+
+    public SafetyRoute7520()
+    {
+
+    }
+}
